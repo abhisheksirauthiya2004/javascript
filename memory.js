e4a97d11238198b08833087ba4aebf3616a402e9
abhisheksirauthiya2004 <133157838+abhisheksirauthiya2004@users.noreply.github.com>
2025-04-06 09:00:21 +0000

    conatiner config

---
+++ memory.js
@@ -0,0 +1,17 @@
+let myYoutubeName ="abhisheksirauthiyadotcom"
+let anotherName = myYoutubeName
+anotherName = "thomas"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email:"user@google.com",
+    upi:"12@ybl"
+}
+
+let userTwo = userOne
+userTwo.email="love12@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
