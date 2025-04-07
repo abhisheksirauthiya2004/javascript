c053d7e165271eca50415c488950b7ecd9b80367
abhisheksirauthiya2004 <133157838+abhisheksirauthiya2004@users.noreply.github.com>
2025-04-07 06:48:45 +0000

    container config

---
+++ maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(100);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4));
+
+const a=123.8443
+// console.log(a.toPrecision(3))
+
+const hunderds=100000
+// console.log(hunderds.toLocaleString('en-IN'))
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.67))
+// console.log(Math.ceil(4.467))
+// console.log(Math.floor(4.467))
+
+
+// console.log(Math.min(4,4,6,7))
+// console.log(Math.max(4,4,6,7))
+// console.log((Math.random()*10)+1)
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()* (max-min+1))+min) 
