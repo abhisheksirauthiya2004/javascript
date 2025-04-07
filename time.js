30b8c0b7ce0b1b254729c53ecf214b8bc46caa0a
abhisheksirauthiya2004 <133157838+abhisheksirauthiya2004@users.noreply.github.com>
2025-04-07 11:19:03 +0000

    container config

---
+++ time.js
@@ -0,0 +1,34 @@
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toUTCString())
+// console.log(myDate.toTimeString())
+// console.log(typeof myDate )
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleString())
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+// // to get time in seconds
+// console.log(Math.floor(Date.now()/100))
+
+let newDate = new Date()
+// // console.log(newDate);
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay())
+
+// console.log(newDate.toLocaleString('default',{
+//     weekday: "long"
+// }))
+
+
